f64f5faecbb06353b8682366bc10f96de826b381
matilda svensson <info@matildasvensson.se>
2023-11-13 16:11:22 +0100

    Made a function that returns tomorrow for tomorrow, and for all other days it returns the name of the weekday

--- src/App.js
+++ src/App.js
@@ -36,8 +36,6 @@ export const App = () => {
   }, []);
 
   const formattedDateOptions = {
-    // weekday: 'long',
-    // year: 'numeric',
     month: 'short',
     day: 'numeric'
   };
@@ -50,13 +48,41 @@ export const App = () => {
     return new Date(timestamp * 1000).toLocaleDateString('en-US', formattedDateOptions);
   }
 
+  function formatWeekday(currentTimestamp, forecastTimestamp) {
+    const tomorrowTimestamp = currentTimestamp + 86400;
+    const tomorrowDate = new Date(tomorrowTimestamp * 1000);
+    const forecastDateObject = new Date(forecastTimestamp * 1000);
+
+    if (
+      forecastDateObject.getDate() === tomorrowDate.getDate()
+      && forecastDateObject.getMonth() === tomorrowDate.getMonth()
+      && forecastDateObject.getFullYear() === tomorrowDate.getFullYear()
+    ) {
+      return 'Tomorrow';
+    } else {
+      return new Date(forecastTimestamp * 1000).toLocaleDateString('en-US', { weekday: 'long' });
+    }
+  }
+
+  const dailyNoonForecasts = weatherForecast?.list?.filter((item) => item.dt_txt.includes('12:00'))
+  console.log('dailynooon', dailyNoonForecasts)
+  console.log('currentweather', currentWeather?.dt)
+
+  console.log(currentWeather.dt)
+  console.log(new Date(currentWeather.dt) * 1000)
+  console.log(formatDate(currentWeather.dt))
+  console.log(new Date(currentWeather.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' }))
+
   return (
     <>
       {currentWeather && currentWeather.weather && currentWeather.weather[0] && (
         <div>
           <h1>{currentWeather.name}</h1>
           <h2>Today</h2>
-          <p>{formatDate(currentWeather.dt)}</p>
+          <div>
+            <p><b>Today</b></p>
+            <p>{formatDate(currentWeather.dt)}</p>
+          </div>
           <p>{currentWeather.weather[0].main}</p>
           <p>{Math.floor(currentWeather.main.temp)} °C</p>
           <p>Sunrise: {formatTime(currentWeather.sys.sunrise)}</p>
@@ -66,18 +92,22 @@ export const App = () => {
 
       <div>
         <h2>Forecast </h2>
-        {weatherForecast?.list?.length ? (
-          weatherForecast.list
-            .filter((item) => item.dt_txt.includes('12:00'))
-            .map((item) => (
-              <div key={item.dt}>
-                <p>{formatDate(item.dt)}</p>
-                <p>{Math.floor(item.main.temp)} °C</p>
-                {/* <p>{new Date(item.dt_txt)}</p> */}
-                <p>{item.weather[0]?.description}</p>
-                <p>feels like {Math.floor(item.main.feels_like)}</p>
-              </div>
-            ))
+        {/* {weatherForecast?.list?.length ? ( */}
+        {dailyNoonForecasts?.length ? (
+          // weatherForecast.list
+          // .filter((item) => item.dt_txt.includes('12:00'))
+          // .map((item) => (
+          dailyNoonForecasts.map((item) => (
+          // {dailyNoonForecasts.map((item) => (
+            <div key={item.dt}>
+              <p><b>{formatWeekday(currentWeather.dt, item.dt)}</b></p>
+              <p>{formatDate(item.dt)}</p>
+              <p>{Math.floor(item.main.temp)} °C</p>
+              {/* <p>{new Date(item.dt_txt)}</p> */}
+              <p>{item.weather[0]?.description}</p>
+              <p>feels like {Math.floor(item.main.feels_like)}</p>
+            </div>
+          ))
         ) : (
           <p>No forecast available</p>
         )}

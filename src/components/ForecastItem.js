694422a3463423d453f4773311b23204a91e0224
matilda svensson <info@matildasvensson.se>
2023-12-04 23:09:13 +0100

    added accordion with hover effect on forecast  item

--- src/components/ForecastItem.js
+++ src/components/ForecastItem.js
@@ -27,19 +27,18 @@ export const ForecastItem = ({ item, currentWeather }) => {
   `
 
   const HeaderContent = styled.div`
+      position: relative;
       display: flex;
-      flex-direction: row;
+      align-items: center;
       justify-content: space-between;
-      align-items: baseline;
       border-bottom: 1px solid black;
       cursor: pointer;
-      transition: 0,4s;
       width: 100%;
-      /* height: 50px; */
-      :hover {
+      transition: background-color 0.4s ease;
+      &:hover {
         background-color: #B72C72;
-      } 
-  `
+      }
+     `
 
   const DateInfo = styled.div`
       display: flex;
@@ -57,22 +56,61 @@ export const ForecastItem = ({ item, currentWeather }) => {
   const Paragraph = styled.p`
       margin: 0 0 12px 0;
   `
+  // const AccordionHeader = styled.button`
+  //   cursor: pointer;
+  //   background-color: grey;
+  //   transition: 0,4s;
+  //   border: none;
+  //   width: 100%;
+  //   max-width: 600px;
+  //   height: 30px;
+  //   /* margin: 0 0 10px 0; */
+  //     :hover {
+  //     background-color: #B72C72;
+  //     }
+  // `
+  // const AccordionContent = styled.div`
+  //   overflow: hidden;
+  //   /* max-height: ${(props) => (props.open ? '100%' : '0')}; */
+  //   display: ${(props) => (props.show ? 'flex' : 'none')};
+  //   width: 100%;
+  // `
+
+  // const Title = styled.p`
+  //   margin: 0;
+  //   color: white;
+  //   font-size: 16px;
+  // `
 
   return (
-    <ForecastContainer key={item.dt}>
-      <HeaderContent onClick={toggleForecast}>
+    <>
+      <ForecastContainer key={item.dt}>
+        <HeaderContent onClick={toggleForecast}>
+          <DateInfo>
+            <button type="button">{showForecast ? '▲' : '▼'}</button>
+            <H2>{formatWeekday(currentWeather.dt, item.dt)}</H2>
+            <Paragraph>{formatDate(item.dt)}</Paragraph>
+          </DateInfo>
+          <Paragraph>{Math.floor(item.main.temp)} °C</Paragraph>
+        </HeaderContent>
+        <ForecastInfo show={showForecast}>
+          <Paragraph>{capitalizeFirstLetter(item.weather[0]?.description)}</Paragraph>
+          <Paragraph>, feels like {Math.floor(item.main.feels_like)}</Paragraph>
+        </ForecastInfo>
+      </ForecastContainer>
+
+      {/* <AccordionHeader onClick={toggleForecast}>
         <DateInfo>
           <button type="button">{showForecast ? '▲' : '▼'}</button>
           <H2>{formatWeekday(currentWeather.dt, item.dt)}</H2>
           <Paragraph>{formatDate(item.dt)}</Paragraph>
         </DateInfo>
         <Paragraph>{Math.floor(item.main.temp)} °C</Paragraph>
-        {/* <button type="button">{showForecast ? '▲' : '▼'}</button> */}
-      </HeaderContent>
-      <ForecastInfo show={showForecast}>
+      </AccordionHeader>
+      <AccordionContent show={showForecast}>
         <Paragraph>{capitalizeFirstLetter(item.weather[0]?.description)}</Paragraph>
         <Paragraph>, feels like {Math.floor(item.main.feels_like)}</Paragraph>
-      </ForecastInfo>
-    </ForecastContainer>
+      </AccordionContent> */}
+    </>
   )
 }
